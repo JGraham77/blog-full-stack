6f26b6b3a011e1e4d0b6cc9795b9dfe88546212b
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-09-06 17:20:45 -0500

    styling thank you page

--- src/client/views/Thanks.tsx
+++ src/client/views/Thanks.tsx
@@ -4,7 +4,7 @@ const Thanks = () => {
     return (
         <div className="row justify-content-center">
             <div className="col-12 col-md-9 col-lg-7">
-                <h1 className="text-center">Thank you for your donation!</h1>
+                <h1 className="text-center m-5 text-danger">Thank you for your donation!</h1>
             </div>
         </div>
     );
